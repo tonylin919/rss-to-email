15c4f3d8280df4300b763c457ab2b693daf25bd9
tonylin919 <tonylin919@gmail.com>
2023-03-20 12:39:59 -0700

    Update Rejected.tsx

--- src/email/Rejected.tsx
+++ src/email/Rejected.tsx
@@ -9,31 +9,13 @@ interface Props {
 
 export default ({ feed, reason }: Props) => {
   return (
-    <Container style={box}>
-      <Text style={text}>
-        <Link style={link} href={feed}>
+    <Container>
+      <Text>
+        <Link href={feed}>
           {feed}
         </Link>
       </Text>
-      <Text style={text}>Reason: {JSON.stringify(reason)}</Text>
+      <Text>Reason: {JSON.stringify(reason)}</Text>
     </Container>
   )
-}
-
-const box = {
-  padding: '16px 32px',
-  backgroundColor: '#c92a2a',
-}
-
-const text = {
-  color: '#fff',
-  fontFamily: 'Dank Mono,Operator Mono,Inconsolata,Fira Mono,ui-monospace,SF Mono,Monaco,Droid Sans Mono,Source Code Pro,monospace',
-  fontSize: '12px',
-  wordBreak: 'break-word' as const,
-  margin: '16px 0',
-}
-
-const link = {
-  color: '#fff',
-  textDecoration: 'underline',
 }
